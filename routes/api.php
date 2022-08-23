7a52088f8df6bdfb9c8338943ca98980f7f674b0
Docteurexplod <delta80@outlook.fr>
2022-08-23 09:59:21 +0200

    update

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BodyZoneController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
+Route::get('/exercice/{id}', [BodyZoneController::class, 'show'])->name('exercices.show');
