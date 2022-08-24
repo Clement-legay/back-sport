80553a3c8e9fc283b6d1d88f6f0541453fd36267
Docteurexplod <delta80@outlook.fr>
2022-08-24 17:39:09 +0200

    update

--- app/Http/Controllers/MediaController.php
+++ app/Http/Controllers/MediaController.php
@@ -106,7 +106,6 @@ class MediaController extends Controller
     }
 
     public function destroy($reference_type, $id) {
-        dd($reference_type, $id);
         if ($reference_type == 'exercice') {
             $result = ExerciceMedia::find($id);
         } else if ($reference_type == 'body_zone') {
