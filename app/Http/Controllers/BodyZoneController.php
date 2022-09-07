7916e47d7537023bac5e3366e9769d728fcaade2
Docteurexplod <delta80@outlook.fr>
2022-09-07 17:46:59 +0200

    update

--- app/Http/Controllers/BodyZoneController.php
+++ app/Http/Controllers/BodyZoneController.php
@@ -111,7 +111,7 @@ class BodyZoneController extends Controller
         if ($bodyZone == null) {
             return response()->json(['message' => 'Body zone not found.'], 404);
         } else {
-            $muscles = $bodyZone->muscles()->orderBy('name', 'asc')->get();
+            $muscles = $bodyZone->muscles()->orderBy('updated_at', 'desc')->get();
             return response()->json(['muscles' => $muscles], 200);
         }
     }
